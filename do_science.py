67ae9c210c087df90bec0fa586a522926cb77fd8
Erik Tollerud <erik.tollerud@gmail.com>
2015-02-23 10:49:57 -0500

    first try

--- do_science.py
+++ do_science.py
@@ -1,2 +1,6 @@
-class A(object):
-    a = 4
+from __future__ import division
+
+import numpy as np
+
+def lorentzian(x, x0, A, gamma):
+    return A / (gamma*(1 + ((x - x0)/gamma)**2))
--- test_do_science.py
+++ test_do_science.py
@@ -1,4 +1,21 @@
-def test_exp():
-    import exp
-    a = exp.A()
-    assert a.a == 4
+from __future__ import division
+
+import numpy as np
+
+
+def test_lorentzian_known_good():
+    from do_science import lorentzian
+
+    assert lorentzian(0, 0, 1, 1) == 1/np.pi
+    assert lorentzian(1, 1, np.pi, 1) == 1
+    assert lorentzian(3, 1, np.pi, 2) == 0.25
+
+
+def test_lorentz_normalized():
+    from scipy.integrate import quad
+    from do_science import lorentzian
+
+    for lorentzian_args in [(1, 1, 1.5), (2, 3, 4), (1.2, 83, 12)]:
+        print lorentzian_args
+        integrand, err = quad(lorentzian, -np.inf, np.inf, args=lorentzian_args)
+        assert abs(integrand - lorentzian_args[1]) < 2*err
