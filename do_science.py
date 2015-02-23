d8a76c43c399dbc2cc6a14d1daa47c2ccb6c1331
Erik Tollerud <erik.tollerud@gmail.com>
2015-02-23 10:52:39 -0500

    fix

--- do_science.py
+++ do_science.py
@@ -3,6 +3,6 @@ from __future__ import division
 import numpy as np
 
 def lorentzian(x, x0, A, gamma):
-    return A / (gamma*(1 + ((x - x0)/gamma)**2))
+    return A / (np.pi*gamma*(1 + ((x - x0)/gamma)**2))
 
 # ... code that uses the `lorentzian` to fit a spectral line goes here ...
